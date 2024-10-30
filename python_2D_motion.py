9f21dc72899e5b4b9d53a42e86942f77350bdd50
Mark Peters <mapeters101@earthlink.net>
2024-10-30 14:19:49 -0400

    Lesson115:  Work on lesson from 1:15 up to 2:30.

--- python_2D_motion.py
+++ python_2D_motion.py
@@ -4,7 +4,7 @@ import matplotlib.gridspec as gridspec
 import matplotlib.animation as animation
 import numpy as np
 
-type=5
+type=6
 
 # Time array
 t0=0
@@ -55,6 +55,13 @@ elif type==5:
     b=500
     x=a+40*t*np.cos(2*np.pi*f*t)
     y=b+40*t*np.sin(2*np.pi*f*t)
+elif type==6:
+    r=200
+    f=1/5
+    a=1000
+    b=500
+    x=a+r*np.cos(2*np.pi*(0.2*t)*t)
+    y=b+r*np.sin(2*np.pi*(0.2*t)*t)
 
 
 ############################## ANIMATION ##############################
